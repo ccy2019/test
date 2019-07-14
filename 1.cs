ae77bca91120b993364902b8c5bb6b57a7ac00e9
ccy2019 <chenyuanxjtu@163.com>
2019-07-14 17:42:31 +0800

    my first git push oprate

--- 1.cs
+++ 1.cs
@@ -2,6 +2,6 @@ class HelloWorld
 {
     static void Main()
 	{
-            System.Console.WriteLine("my first C# program");
+            System.Console.WriteLine("my first C# program");/*for first git push*/
 	}
 }
